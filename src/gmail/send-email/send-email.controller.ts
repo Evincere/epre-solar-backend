7a9fcfdacd4a04684c098beea7dd9c47f8479929
Semper Evincere <spereyra.jus@gmail.com>
2024-08-21 18:23:01 -0300

    envia mail al detectar el cambio de potencia de paneles en la api solar

--- src/gmail/send-email/send-email.controller.ts
+++ src/gmail/send-email/send-email.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { Auth } from 'googleapis';
 import * as dotenv from 'dotenv';
 import { MailService } from './send-email.service';
@@ -19,6 +19,15 @@ export class SendEmailController {
         'spereyra.jus@gmail.com',
         'Información de tu navegación',
         '<h1>Gracias por navegar por nuestro sitio!</h1><p>Aquí tienes la información...</p>'
+      );
+    }
+
+    @Get('send-email-change')
+    async sendEmailChangeCapacityInApi(@Query('newPanelCapacityW') newPanelCapacityW: number) {
+      await this.mailService.sendEmail(
+        'spereyra.jus@gmail.com',
+        'Información de actualización',
+        `<h1>Actualización cambio en API solar</h1><p>Google ha actualizado la potencia de los paneles como base para sus calculos solares.</p><p>La nueva potencia es de ${newPanelCapacityW} w`
       );
     }
 }
