42e6aec85fb6c7a6b5fa548a870b09a5539731b1
Semper Evincere <spereyra.jus@gmail.com>
2024-08-26 17:12:45 -0300

    controller para envio de email

--- src/gmail/send-email/send-email.controller.ts
+++ src/gmail/send-email/send-email.controller.ts
@@ -8,26 +8,31 @@ const { OAuth2Client } = Auth;
 
 @Controller('gmail')
 export class SendEmailController {
+  constructor(private readonly mailService: MailService) {}
 
-    constructor(private readonly mailService: MailService) {
-    }
-    
-    @Get('send-email')
-    async finalizarNavegacion() {
-      //todo: lógica para obtener la información a enviar por email ...
-      await this.mailService.sendEmail(
-        'spereyra.jus@gmail.com',
-        'Información de tu navegación',
-        '<h1>Gracias por navegar por nuestro sitio!</h1><p>Aquí tienes la información...</p>'
-      );
+  @Get('send-email')
+  async finalizarNavegacion(@Query('email') email: string) {
+    if (!email) {
+      throw new Error('El email es requerido');
     }
 
-    @Get('send-email-change')
-    async sendEmailChangeCapacityInApi(@Query('newPanelCapacityW') newPanelCapacityW: number) {
-      await this.mailService.sendEmail(
-        'spereyra.jus@gmail.com',
-        'Información de actualización',
-        `<h1>Actualización cambio en API solar</h1><p>Google ha actualizado la potencia de los paneles como base para sus calculos solares.</p><p>La nueva potencia es de ${newPanelCapacityW} w`
-      );
-    }
+    await this.mailService.sendEmail(
+      email, 
+      'Información de tu navegación',
+      '<h1>Gracias por navegar por nuestro sitio!</h1><p>Aquí tienes la información...</p>',
+    );
+
+    return { message: 'Email enviado exitosamente' };
+  }
+
+  @Get('send-email-change')
+  async sendEmailChangeCapacityInApi(
+    @Query('newPanelCapacityW') newPanelCapacityW: number,
+  ) {
+    await this.mailService.sendEmail(
+      'spereyra.jus@gmail.com',
+      'Información de actualización',
+      `<h1>Actualización cambio en API solar</h1><p>Google ha actualizado la potencia de los paneles como base para sus calculos solares.</p><p>La nueva potencia es de ${newPanelCapacityW} w`,
+    );
+  }
 }
